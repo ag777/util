cb0e95dd7c19091601db98a664c2ebcb824a691f
ag777 <aaaggg777@qq.com>
2025-03-13 20:17:48 +0800

    增加 补充科学计算工具类和异常模型
    - 新增 CalculateHelper 类，提供精确的浮点数计算功能
    - 新增 DivisorIsZeroException 类，用于处理除数为零的异常
    - 新增 MathUtils 类，提供常用的数学计算工具方法
    - 新增 ValidateException 类，用于参数验证异常

---
+++ src/main/java/github/ag777/util/lang/exception/model/ValidateException.java
@@ -0,0 +1,55 @@
+package github.ag777.util.lang.exception.model;
+
+/**
+ * 验证异常(验证参数过程，判断出的异常)
+ * @author ag777
+ *
+ * @version create on 2018年05月17日,last modify at 2020年04月24日
+ */
+public class ValidateException extends Exception {
+
+	private static final long serialVersionUID = -1358796764058245553L;
+
+	private String extraMsg;	//拓展信息
+	private Boolean isException;	// 可以用于确定是系统异常还是输入异常
+
+	public String getExtraMsg() {
+		return extraMsg;
+	}
+
+	public ValidateException(String message){
+		super(message);
+		this.isException = false;
+	}
+
+	public ValidateException(String message, String extraMsg){
+		super(message);
+		this.extraMsg = extraMsg;
+		this.isException = false;
+	}
+
+	public ValidateException(String message, Throwable cause) {
+		super(message, cause);
+		this.isException = cause != null;
+	}
+
+	public ValidateException(String message, String extraMsg, Throwable cause) {
+		super(message, cause);
+		this.extraMsg = extraMsg;
+		this.isException = cause != null;
+	}
+
+	public Boolean getException() {
+		return isException;
+	}
+
+	public ValidateException setException(Boolean exception) {
+		isException = exception;
+		return this;
+	}
+
+	public ValidateException setMessage(String newMessage) {
+		return new ValidateException(newMessage, getCause());
+	}
+
+}
