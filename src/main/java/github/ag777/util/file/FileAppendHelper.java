a4e549063698587966114643b92ec50ed89e5618
ag777 <aaaggg777@qq.com>
2025-04-05 18:21:16 +0800

    增加 补充文件追加辅助类和路径工具类
    - 新增 FileAppendHelper 类，用于高效追加内容到文件
    - 新增 PathUtils 类，提供路径相关工具方法
    - FileAppendHelper支持指定字符集和自动换行
    - PathUtils 包含获取资源文件流、src路径等实用方法

---
+++ src/main/java/github/ag777/util/file/FileAppendHelper.java
@@ -0,0 +1,112 @@
+package github.ag777.util.file;
+
+import github.ag777.util.lang.IOUtils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * 文件追加辅助类
+ * 
+ * @author ag777
+ * @version create on 2020年05月29日,last modify at 2024年11月23日
+ */
+public class FileAppendHelper implements AutoCloseable {
+
+	private final Path to;
+	private final Charset charset;
+	private volatile BufferedWriter writer;	//该变量不可直接使用，调用getWritter()方法获取
+
+	/**
+	 * 构造函数，初始化FileAppendHelper对象，默认使用UTF-8字符集
+	 * @param file 要写入的文件对象
+	 */
+	public FileAppendHelper(File file) {
+	    this.charset = StandardCharsets.UTF_8;
+	    this.to = file.toPath();
+	}
+
+	/**
+	 * 构造函数，初始化FileAppendHelper对象，允许指定字符集
+	 * @param file 要写入的文件对象
+	 * @param charset 使用的字符集
+	 */
+	public FileAppendHelper(File file, Charset charset) {
+	    this.charset = charset;
+	    this.to = file.toPath();
+	}
+
+	/**
+	 * 追加一行内容到文件中，自动添加换行符
+	 * @param content 要追加的内容
+	 * @return FileAppendHelper对象，支持链式调用
+	 * @throws IOException 如果写入文件时发生I/O错误
+	 */
+	public FileAppendHelper appendLine(String content) throws IOException {
+	    return append(content+"\r\n");
+	}
+
+	/**
+	 * 追加内容到文件中
+	 * @param content 要追加的内容
+	 * @return FileAppendHelper对象，支持链式调用
+	 * @throws IOException 如果写入文件时发生I/O错误
+	 */
+	public FileAppendHelper append(String content) throws IOException {
+	    getWriter().write(content);
+	    return this;
+	}
+
+	/**
+	 * 获取BufferedWriter对象，用于写入指定路径的文件
+	 * @param path 文件路径
+	 * @param charset 字符集
+	 * @return BufferedWriter对象，用于写入文件
+	 * @throws IOException 如果创建BufferedWriter时发生I/O错误
+	 */
+	public static BufferedWriter getBufferedWriter(Path path, Charset charset) throws IOException {
+	    return Files.newBufferedWriter(path, charset,
+	            StandardOpenOption.CREATE, StandardOpenOption.APPEND); // 追加
+	}
+
+	/**
+	 * 获取当前对象的BufferedWriter对象，如果未初始化则进行初始化
+	 * @return BufferedWriter对象，用于写入文件
+	 * @throws IOException 如果初始化BufferedWriter时发生I/O错误
+	 */
+	private BufferedWriter getWriter() throws IOException {
+	    if(writer == null) {
+	        synchronized (FileAppendHelper.class) {
+	            if(writer == null) {
+	                writer = getBufferedWriter(to, charset);
+	            }
+	        }
+	    }
+	    return writer;
+	}
+
+	/**
+	 * 刷新文件写入器，确保所有数据写入文件
+	 * @throws IOException 如果刷新写入器时发生I/O错误
+	 */
+	public void flush() throws IOException {
+	    if (writer != null) {
+	        writer.flush();
+	    }
+	}
+
+	/**
+	 * 关闭文件写入器，释放系统资源
+	 */
+	@Override
+	public void close() {
+	    IOUtils.close(writer);
+	}
+	
+}
---
+++ src/main/java/github/ag777/util/file/PathUtils.java
@@ -0,0 +1,125 @@
+package github.ag777.util.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Paths;
+
+/**
+ * 路径工具类
+ * 
+ * @author ag777
+ * @version last modify at 2024年05月28日
+ */
+public class PathUtils {
+	
+	/**
+	 * 获取resource文件夹下资源文件的io流
+	 * @param clazz clazz
+	 * @param filePath filePath
+	 * @return 资源文件的io流
+	 */
+	public static InputStream getAsStream(String filePath, Class<?> clazz) {
+		if(clazz == null) {
+			clazz = PathUtils.class;
+		}
+		return clazz.getClassLoader().getResourceAsStream(filePath);
+	}
+	
+	/**
+	 * 获取src路径，必须传类，如果用这个类会得到lib包的路径
+	 * <p>
+	 * 	返回结果如:/D:/tools/programming/eclipse_neon/Utils-Java/bin/
+	 * </p>
+	 * 
+	 * @param clazz clazz
+	 * @return src路径
+	 */
+	public static String srcPath(Class<?> clazz) {
+		if(clazz == null) {
+			clazz = PathUtils.class;
+		}
+		java.net.URL url = clazz.getProtectionDomain().getCodeSource().getLocation();
+		String filePath = url.getPath();
+		try {
+			filePath = URLDecoder.decode(filePath, StandardCharsets.UTF_8);
+		} catch (Exception e) {
+//			e.printStackTrace();
+		}
+		File file = new File(filePath);
+		if(!file.isDirectory()) {	//可能是jar包，因为结果是/xxxx/xx.jar,所以我们要去得到jar包的同级路径
+			return file.getParent();
+		}
+		try {
+			// 尝试标准化路径
+			return new File(filePath).getCanonicalPath();
+		} catch (IOException e) {
+			return filePath;
+		}
+	}
+	
+	/**
+	 * 获取src路径中的子路径
+	 * <p>
+	 * 	srcPath(PathUtils.class, "config/") 得到xxx/src/config/
+	 * </p>
+	 * 
+	 * @param clazz clazz
+	 * @param subPath subPath
+	 * @return src路径
+	 */
+	public static String srcPath(Class<?> clazz, String subPath) {
+		return new File(srcPath(clazz), subPath).getAbsolutePath();
+	}
+	
+	/**
+	 * 获取相对路径
+	 * <p>
+	 * <pre>{@code
+	 *  PathUtils.getRelativizePath("f:\\a\\b.txt","f:\\").toString(); => "a\\b.txt"
+	 *  }</pre>
+	 * 
+	 * @param targetPath 目标路径(需要转化为相对路径的绝对路径)
+	 * @param basePath 基础路径(标尺)
+	 * @return 相对路径
+	 */
+	public static String getRelativizePath(String targetPath, String basePath) {
+		return Paths.get(basePath).relativize(Paths.get(targetPath)).toString();
+	}
+
+	/**
+	 * 类是否在jar包中
+	 * @param clazz 参照类，用于获取路径
+	 * @return 当前代码是否处于jar包中
+	 */
+	public static boolean inJar(Class<?> clazz) {
+		String protocol = clazz.getResource("").getProtocol();
+		return "jar".equals(protocol);
+	}
+
+
+	/**
+	 * 获取给定类的根路径。如果类是在JAR文件中，则返回运行时的根路径；如果类是在文件系统中，则返回类的代码源位置路径。
+	 * @param clazz 任意类，用于确定根路径。
+	 * @return 根路径的字符串表示，末尾包含文件分隔符。
+	 */
+	public static String getRootPath(Class<?> clazz) {
+	    String path;
+	    if (inJar(clazz)) {
+	        // 当类在JAR中时，获取当前运行时的根路径
+	        path = FileSystems.getDefault().getPath("").toAbsolutePath().toString();
+	    } else {
+	        // 当类在文件系统中时，获取类的代码源位置路径
+	        path = clazz.getProtectionDomain().getCodeSource().getLocation().getPath();
+	        path = new File(path).getAbsolutePath();
+	    }
+        // 尝试将路径URL解码为UTF-8格式
+        path = URLDecoder.decode(path, StandardCharsets.UTF_8);
+        // 返回解码后的路径的父路径，并在末尾添加一个文件分隔符
+	    return path + File.separator;
+	}
+
+}
