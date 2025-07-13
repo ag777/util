420ec8573af1aad6e7dcf30b81ce515cd1c3e2f9
ag777 <aaaggg777@qq.com>
2025-07-13 18:12:49 +0800

    修改 优化 ValidateException 类功能
    - 添加自定义消息字段和相关方法，支持修改错误消息
    - 重写 getMessage 方法，返回自定义消息
    - 新增 getOriginalMessage 方法，获取原始错误消息
    - 添加 serialVersionUID 以保证序列化兼容性

--- src/main/java/github/ag777/util/lang/exception/model/ValidateException.java
+++ src/main/java/github/ag777/util/lang/exception/model/ValidateException.java
@@ -1,17 +1,21 @@
 package github.ag777.util.lang.exception.model;
 
+import java.io.Serial;
+
 /**
  * 验证异常(验证参数过程，判断出的异常)
  * @author ag777
  *
- * @version create on 2018年05月17日,last modify at 2020年04月24日
+ * @version create on 2018年05月17日,last modify at 2025年07月13日
  */
 public class ValidateException extends Exception {
 
+	@Serial
 	private static final long serialVersionUID = -1358796764058245553L;
 
 	private String extraMsg;	//拓展信息
 	private Boolean isException;	// 可以用于确定是系统异常还是输入异常
+	private String customMessage;	// 自定义消息字段，用于支持消息修改
 
 	public String getExtraMsg() {
 		return extraMsg;
@@ -19,22 +23,26 @@ public class ValidateException extends Exception {
 
 	public ValidateException(String message){
 		super(message);
+		this.customMessage = message;
 		this.isException = false;
 	}
 
 	public ValidateException(String message, String extraMsg){
 		super(message);
+		this.customMessage = message;
 		this.extraMsg = extraMsg;
 		this.isException = false;
 	}
 
 	public ValidateException(String message, Throwable cause) {
 		super(message, cause);
+		this.customMessage = message;
 		this.isException = cause != null;
 	}
 
 	public ValidateException(String message, String extraMsg, Throwable cause) {
 		super(message, cause);
+		this.customMessage = message;
 		this.extraMsg = extraMsg;
 		this.isException = cause != null;
 	}
@@ -48,8 +56,31 @@ public class ValidateException extends Exception {
 		return this;
 	}
 
+	/**
+	 * 重写getMessage方法，返回自定义消息
+	 * @return 当前的错误消息
+	 */
+	@Override
+	public String getMessage() {
+		return customMessage != null ? customMessage : super.getMessage();
+	}
+
+	/**
+	 * 直接修改当前异常对象的message，不创建新对象
+	 * @param newMessage 新的错误信息
+	 * @return 当前异常对象
+	 */
 	public ValidateException setMessage(String newMessage) {
-		return new ValidateException(newMessage, getCause());
+		this.customMessage = newMessage;
+		return this;
+	}
+
+	/**
+	 * 获取原始的Exception消息（来自父类）
+	 * @return 原始消息
+	 */
+	public String getOriginalMessage() {
+		return super.getMessage();
 	}
 
 }
